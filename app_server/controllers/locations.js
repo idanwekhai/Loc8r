83303c816fa4a212726d3e2136b0f576f6b0f496
idanwekhai <kelvinidanwekhai@gmail.com>
2017-04-18 00:27:30 +0100

    Commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -10,5 +10,5 @@ module.exports.locationinfo = function(req, res, next) {
 
 /*Get 'review' page*/
 module.exports.addReview = function(req, res, next) {
-  res.render('index', { title: 'Add Review' });
+  res.render('location-review-form', { title: 'Add Review' });
 };
