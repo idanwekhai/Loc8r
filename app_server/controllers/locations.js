d55b672ba120f3727734085486d88084721309f1
idanwekhai <kelvinidanwekhai@gmail.com>
2017-04-20 00:47:09 +0100

    Moved some data out of the views

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,31 @@
 /*Get 'home' page*/
 module.exports.homelist = function(req, res, next) {
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', { 
+  	title: 'Loc8r - find a place to work with wifi',
+  	pageHeader: {
+  		title: 'Loc8r',
+  		strapline: 'Find place to work with wifi near you!'
+  	},
+     locations: [{
+        name: "Starcups",
+        address: "125 High Street, Reading, RG6 1PS",
+        ratings:  3,
+        facilities: ["Hot drinks", "Food", "Premium wifi"],
+        distance: "100m"
+       },{
+       	name: "White House",
+        address: "Ile-ife Street, Along Moremi, REX 150",
+        ratings: 4,
+        facilities: ["Cold drinks", "Snacks", "Burgers", "Premium wifi"],
+        distance: "120m"
+       },{
+        name: "Afrika",
+        address: "Angola Hall, Afrikas Block, D Buttery",
+        ratings: 2,
+        facilities: ["Cold drinks", "Delicious Beans"],
+        distance: "90m"
+     }]
+  });
 };
 
 /*Get 'location info' page*/
