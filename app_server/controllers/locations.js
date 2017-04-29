77276b4eb5317c0719bf15b9ca5848bcf80d225c
idanwekhai <kelvinidanwekhai@gmail.com>
2017-04-29 02:24:50 +0100

    Fixed some issues

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,7 @@ var apiOptions = {
     server : "http://localhost:3000"
    };
    if (process.env.NODE_ENV === 'production') {
-      apiOptions.server = "https://getting-mean-loc8r.herokuapp.com";
+      apiOptions.server = "https://stark-hollows-61646.herokuapp.com";
 }
 
 var renderHomePage = function(req, res, responseBody){
